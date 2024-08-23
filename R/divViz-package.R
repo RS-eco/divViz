2885463400576e78e0b53b53ffdf3404d5006b48
RS-eco <rs-eco@posteo.de>
2024-08-23 11:31:22 +0200

    Update data.

--- R/divViz-package.R
+++ R/divViz-package.R
@@ -2,22 +2,10 @@
 #' 
 #' @name divViz package
 #' @aliases divVizpackage
-#' @docType package
 #' @title divViz: Visualisation tool for biodiversity data
 #' @description R package for Shiny visualisation tool of biodiversity data.
 #' @author RS-eco
 #' @keywords package
-#' 
-NULL
-#'
-#' @docType data
-#' @name bavaria
-#' @title administrative boundary of bavaria
-#' @description GADM outline of Bavaria, Germany
-#' @usage data(bavaria)
-#' @details Administrative boundary of Bavaria, Germany derived from GADM data.
-#' @format A \code{data.frame} with 1 observation and 13 variables.
-#' @source This data has been obtained from: \itemize{\item \url{https://www.gadm.org/data.html}}
 #' 
 NULL
 #'
